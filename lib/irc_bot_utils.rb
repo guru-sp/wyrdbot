ef9842b1d6ea85266acddeac1838ae645056f832
Bruno Coimbra <bbcoimbra@gmail.com>
2009-10-28 10:41:47 -0200

    irc_bot_utils: incluido método de tradução

--- lib/irc_bot_utils.rb
+++ lib/irc_bot_utils.rb
@@ -27,4 +27,19 @@ module Utils
         doc = Nokogiri::HTML(open(URI.escape(url)))
         doc.xpath("//div[@class='cambio']/ul/li[1]/p/span[2]").text
     end
+
+    def translate(sl, dl, str)
+        host = "http://translate.google.com"
+        path = "/translate_a/t?"
+        qry = ["client=t",
+            "text=#{str}",
+            "sl=#{sl}",
+            "tl=#{dl}",
+            "oc=0",
+            "pc=0"] * '&'
+        url = host + path + qry
+        doc = Nokogiri::HTML(open(URI.encode(url)))
+        ret = doc.xpath("//p").text
+        ret
+    end
 end
