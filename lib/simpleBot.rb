8f4ebe6cdf0521b68e077866008b718c4e9f215b
Bruno Coimbra <bbcoimbra@gmail.com>
2009-10-28 10:44:49 -0200

    simpleBot: incluido método de tradução modificação na identação (não gosto de código identado com 2 espaços, prefiro 4)

--- lib/simpleBot.rb
+++ lib/simpleBot.rb
@@ -13,65 +13,72 @@ class SimpleIrcBot
     include GoogleSearch
     include Utils
 
-  def initialize(server, port, channel, nick = 'wyrd')
-    @channel = channel
-    @socket = TCPSocket.open(server, port)
-    say "NICK #{nick}"
-    say "USER #{nick} 0 * #{nick.capitalize}"
-    say "JOIN ##{@channel}"
-    #say_to_chan "#{1.chr}ACTION is here to help#{1.chr}"
-  end
+    def initialize(server, port, channel, nick = 'wyrd')
+        @channel = channel
+        @socket = TCPSocket.open(server, port)
+        say "NICK #{nick}"
+        say "USER #{nick} 0 * #{nick.capitalize}"
+        say "JOIN ##{@channel}"
+        #say_to_chan "#{1.chr}ACTION is here to help#{1.chr}"
+    end
 
-  def say(msg)
-    puts msg
-    @socket.puts msg
-  end
+    def say(msg)
+        $stdout.write(msg + "\n")
+        @socket.puts msg
+    end
 
-  def say_to_chan(msg)
-    say "PRIVMSG ##{@channel} :#{msg}"
-  end
+    def say_to_chan(msg)
+        say "PRIVMSG ##{@channel} :#{msg}"
+    end
 
-  def run
-    until @socket.eof? do
-      msg = @socket.gets
+    def run
+        until @socket.eof? do
+            msg = @socket.gets
 
-      if msg.match(/^PING :(.*)$/)
-        say "PONG #{$~[1]}"
-        $stderr.write(msg)
-        next
-      end
+            if msg.match(/^PING :(.*)$/)
+                say "PONG #{$~[1]}"
+                $stderr.write(msg)
+                next
+            end
 
-      $stdout.write(msg)
-      if msg.match(/:([^!]+)!.*PRIVMSG ##{@channel} :(.*)$/)
-        nick = $~[1]
-        content = $~[2]
+            if msg.match(/:([^!]+)!.*PRIVMSG ##{@channel} :(.*)$/)
+                nick = $~[1]
+                content = $~[2]
 
-        #put matchers here
-        if content.match(/^!([^:]+):(.*)$/)
-            target = $~[1]
-            query = $~[2]
-            query.squeeze!(' ')
+                #put matchers here
+                if content.match(/^!([^:]+):(.*)$/)
+                    target = $~[1]
+                    query = $~[2]
+                    query.squeeze!(' ')
+                    query.strip!
 
-            case target
-            when 'google' then say_to_chan(google_search(query))
-            when 'doc' then say_to_chan("Documentação: #{query}")
-            when 'dolar' then say_to_chan(cotacao_dolar)
-            else say_to_chan("Comando não reconhecido!")
+                    case target
+                    when 'google' then say_to_chan(google_search(query))
+                    when 'doc' then say_to_chan("Documentação: #{query}")
+                    when 'dolar' then say_to_chan(cotacao_dolar)
+                    when /^translate/
+                        if target =~ /^translate-(..)-(..)/
+                            trad = translate($~[1], $~[2], query)
+                            say_to_chan("Tradução: #{trad}")
+                        else
+                            say_to_chan("Não pude traduzir")
+                        end
+                    else say_to_chan("Se você pedir direito, talvez eu te ajude!")
+                    end
+                end
+                if content.match(/[Bb]om [Dd]ia/)
+                  say_to_chan("Bom dia, #{nick}!")
+                end
+                if content.match(/[Bb]oa [Tt]arde/)
+                   say_to_chan("Boa tarde, #{nick}!")
+                end
             end
         end
-        if content.match(/[Bb]om [Dd]ia/)
-          say_to_chan("Bom dia, #{nick}!")
-        end
-        if content.match(/[Bb]oa [Tt]arde/)
-          say_to_chan("Boa tarde, #{nick}!")
-        end
-      end
     end
-  end
 
-  def quit
-    say "PART ##{@channel} :Saindo!"
-    say 'QUIT'
-  end
+    def quit
+        say "PART ##{@channel} :Saindo!"
+        say 'QUIT'
+    end
 end
 
