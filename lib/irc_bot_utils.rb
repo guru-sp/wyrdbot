5e246039a9e3217e1a95b9c21a9210ecc37bd07f
PotHix <pothix@pothix.com>
2011-04-09 17:37:02 -0300

    Correcting indentation to use 2 spaces

--- lib/irc_bot_utils.rb
+++ lib/irc_bot_utils.rb
@@ -4,83 +4,81 @@ require 'nokogiri'
 require 'open-uri'
 
 module GoogleSearch
+  G_SITE = 'http://www.google.com.br'
+  G_PATH = '/search'
+  G_VARS = 'q'
 
-    G_SITE = 'http://www.google.com.br'
-    G_PATH = '/search'
-    G_VARS = 'q'
-
-    def google_search(params)
-        url = G_SITE + G_PATH + '?' + G_VARS + '=' + params
-        url = URI.escape(url)
-        doc = Nokogiri::HTML(open(url))
-        str = doc.css('h3.r').to_s
-        urls = str.scan(/<a href="([^"]+)"/)
-        urls.flatten!
-        urls[0..1].join(' ')
-    end
-
+  def google_search(params)
+    url = G_SITE + G_PATH + '?' + G_VARS + '=' + params
+    url = URI.escape(url)
+    doc = Nokogiri::HTML(open(url))
+    str = doc.css('h3.r').to_s
+    urls = str.scan(/<a href="([^"]+)"/)
+    urls.flatten!
+    urls[0..1].join(' ')
+  end
 end
 
 module Utils
-    def cotacao_dolar
-        url = 'http://economia.uol.com.br/cotacoes/cambio/dolar-comercial-estados-unidos-principal.jhtm'
-        doc = Nokogiri::HTML(open(URI.escape(url)))
-        doc.xpath("//div[@class='cambio']/ul/li[1]/p/span[2]").text
-    end
+  def cotacao_dolar
+    url = 'http://economia.uol.com.br/cotacoes/cambio/dolar-comercial-estados-unidos-principal.jhtm'
+    doc = Nokogiri::HTML(open(URI.escape(url)))
+    doc.xpath("//div[@class='cambio']/ul/li[1]/p/span[2]").text
+  end
 
-    def translate(sl, dl, str)
-        host = "http://translate.google.com"
-        path = "/translate_a/t?"
-        qry = ["client=t",
-            "text=#{str}",
-            "sl=#{sl}",
-            "tl=#{dl}",
-            "oc=0",
-            "pc=0"] * '&'
-        url = host + path + qry
-        doc = Nokogiri::HTML(open(URI.encode(url)))
-        ret = doc.xpath("//p").text
-        ret
-    end
+  def translate(sl, dl, str)
+    host = "http://translate.google.com"
+    path = "/translate_a/t?"
+    qry = ["client=t",
+      "text=#{str}",
+    "sl=#{sl}",
+    "tl=#{dl}",
+    "oc=0",
+      "pc=0"] * '&'
+    url = host + path + qry
+    doc = Nokogiri::HTML(open(URI.encode(url)))
+    ret = doc.xpath("//p").text
+    ret
+  end
 end
 
 module Greetings
-    def day_part
-	cur_hour = Time::now.hour
-	return :morning if cur_hour >= 5 and cur_hour < 12
-	return :afternoon if cur_hour >= 12 and cur_hour < 18
-	return :night if cur_hour >= 18 or cur_hour < 5
-    end
+  def day_part
+    cur_hour = Time::now.hour
+    return :morning if cur_hour >= 5 and cur_hour < 12
+    return :afternoon if cur_hour >= 12 and cur_hour < 18
+    return :night if cur_hour >= 18 or cur_hour < 5
+  end
 
-    def greet(greeting, nick)
-	case greeting
-	when /[Bb]om [Dd]ia/
-	    case day_part
-	    when :morning
-		"Bom dia, #{nick}!"
-	    when :afternoon
-		"Boa tarde, #{nick}. Ainda não almoçou? São #{Time::now.strftime("%H:%M")}"
-	    when :night
-		"Boa noite, #{nick}! Acabou de acordar? O dia já acabou."
-	    end
-	when /[Bb]oa [Tt]arde/
-	    case day_part
-	    when :morning
-		"Bom dia, #{nick}! Você está adiantado."
-	    when :afternoon
-		"Boa tarde, #{nick}!"
-	    when :night
-		"Boa noite, #{nick}! Já escureceu são #{Time::now.strftime("%H:%M")}."
-	    end
-	when /[Bb]oa [Nn]oite/
-	    case day_part
-	    when :morning
-		"Bom dia, #{nick}! Quanta pressa o dia acabou de começar."
-	    when :afternoon
-		"Boa tarde, #{nick}. Ainda não escureceu são #{Time::now.strftime("%H:%M")}."
-	    when :night
-		"Boa noite, #{nick}!"
-	    end
-	end
+  def greet(greeting, nick)
+    case greeting
+    when /[Bb]om [Dd]ia/
+      case day_part
+      when :morning
+        "Bom dia, #{nick}!"
+      when :afternoon
+        "Boa tarde, #{nick}. Ainda não almoçou? São #{Time::now.strftime("%H:%M")}"
+      when :night
+        "Boa noite, #{nick}! Acabou de acordar? O dia já acabou."
+      end
+    when /[Bb]oa [Tt]arde/
+      case day_part
+      when :morning
+        "Bom dia, #{nick}! Você está adiantado."
+      when :afternoon
+        "Boa tarde, #{nick}!"
+      when :night
+        "Boa noite, #{nick}! Já escureceu são #{Time::now.strftime("%H:%M")}."
+      end
+    when /[Bb]oa [Nn]oite/
+      case day_part
+      when :morning
+        "Bom dia, #{nick}! Quanta pressa o dia acabou de começar."
+      when :afternoon
+        "Boa tarde, #{nick}. Ainda não escureceu são #{Time::now.strftime("%H:%M")}."
+      when :night
+        "Boa noite, #{nick}!"
+      end
     end
+  end
 end
--- lib/simple_bot.rb
+++ lib/simple_bot.rb
@@ -1,83 +1,79 @@
 # encoding: utf-8
 #
-# Original code by Kevin Glowacz 
+# Original code by Kevin Glowacz
 # found in http://github.com/kjg/simpleircbot
-# at 2009-10-26 by bbcoimbra
-#
 
 require 'socket'
 require 'irc_bot_utils'
 
 class SimpleIrcBot
+  include GoogleSearch
+  include Utils
+  include Greetings
 
-    include GoogleSearch
-    include Utils
-    include Greetings
+  def initialize(server, port, channel, nick = 'wyrd')
+    @channel = channel
+    @socket = TCPSocket.open(server, port)
+    say "NICK #{nick}"
+    say "USER #{nick} 0 * #{nick.capitalize}"
+    say "JOIN ##{@channel}"
+    #say_to_chan "#{1.chr}ACTION is here to help#{1.chr}"
+  end
 
-    def initialize(server, port, channel, nick = 'wyrd')
-        @channel = channel
-        @socket = TCPSocket.open(server, port)
-        say "NICK #{nick}"
-        say "USER #{nick} 0 * #{nick.capitalize}"
-        say "JOIN ##{@channel}"
-        #say_to_chan "#{1.chr}ACTION is here to help#{1.chr}"
-    end
+  def say(msg)
+    $stdout.write(msg + "\n")
+    @socket.puts msg
+  end
 
-    def say(msg)
-        $stdout.write(msg + "\n")
-        @socket.puts msg
-    end
+  def say_to_chan(msg)
+    say "PRIVMSG ##{@channel} :#{msg}"
+  end
 
-    def say_to_chan(msg)
-        say "PRIVMSG ##{@channel} :#{msg}"
-    end
+  def run
+    until @socket.eof? do
+      msg = @socket.gets
 
-    def run
-        until @socket.eof? do
-            msg = @socket.gets
+      if msg.match(/^PING :(.*)$/)
+        say "PONG #{$~[1]}"
+        $stderr.write(msg)
+        next
+      end
 
-            if msg.match(/^PING :(.*)$/)
-                say "PONG #{$~[1]}"
-                $stderr.write(msg)
-                next
+      if msg.match(/:([^!]+)!.*PRIVMSG ##{@channel} :(.*)$/)
+        nick = $~[1]
+        content = $~[2]
+
+        #put matchers here
+        if content.match(/^!([^:]+):(.*)$/)
+          target = $~[1]
+          query = $~[2]
+          query.squeeze!(' ')
+          query.strip!
+
+          case target
+          when 'google' then say_to_chan(google_search(query))
+          when 'doc' then say_to_chan("Documentação: #{query}")
+          when 'dolar' then say_to_chan(cotacao_dolar)
+          when /^t/
+            if target =~ /^t-(..)-(..)/
+              trad = translate($~[1], $~[2], query)
+            say_to_chan("Trad: #{trad}")
+            else
+              say_to_chan("Não pude traduzir")
             end
-
-            if msg.match(/:([^!]+)!.*PRIVMSG ##{@channel} :(.*)$/)
-                nick = $~[1]
-                content = $~[2]
-
-                #put matchers here
-                if content.match(/^!([^:]+):(.*)$/)
-                    target = $~[1]
-                    query = $~[2]
-                    query.squeeze!(' ')
-                    query.strip!
-
-                    case target
-                    when 'google' then say_to_chan(google_search(query))
-                    when 'doc' then say_to_chan("Documentação: #{query}")
-                    when 'dolar' then say_to_chan(cotacao_dolar)
-                    when /^t/
-                        if target =~ /^t-(..)-(..)/
-                            trad = translate($~[1], $~[2], query)
-                            say_to_chan("Trad: #{trad}")
-                        else
-                            say_to_chan("Não pude traduzir")
-                        end
-                    else say_to_chan("Se você pedir direito, talvez eu te ajude!")
-                    end
-                    next
-                end
-
-		greeting = greet(content, nick)
-		say_to_chan(greeting) if greeting
-            end
+          else say_to_chan("Se você pedir direito, talvez eu te ajude!")
+          end
+          next
         end
-    end
 
-    def quit
-        say "PART ##{@channel} :Saindo!"
-        say 'QUIT'
+        greeting = greet(content, nick)
+        say_to_chan(greeting) if greeting
+      end
     end
+  end
+
+  def quit
+    say "PART ##{@channel} :Saindo!"
+    say 'QUIT'
+  end
 end
-
