ce84dbac41ff470e1f3842101f4432c54b6c1ce7
PotHix <pothix@pothix.com>
2011-08-25 13:54:04 -0300

    New feature to add motorcycle pictures :D

--- spec/simple_bot/quote_spec.rb
+++ spec/simple_bot/quote_spec.rb
@@ -1,26 +1,12 @@
 # encoding: utf-8
 require "spec_helper"
 
-describe "SimpleIrcBot::Quote" do
-  subject { SimpleIrcBot::Quote.new("I'm testing if this thing works...") }
+describe "SimpleIrcBot::Motorcycle" do
+  subject { SimpleIrcBot::Motorcycle.new("http://myrockerfxcw.com/wp-content/uploads/2010/05/IMAG0083.jpg") }
 
-  it "should add a new phrase to the phrases file" do
+  it "should add a new motorcycle quote to the motorcycles file" do
     subject.add!
     phrases_file = subject.class.file
-    phrases_file["quotes"][phrases_file["quotes"].size - 1].should == subject.phrase
-  end
-
-  it "should return a random phrase" do
-    not_so_random_number = 0
-    subject.class.should_receive(:rand).and_return(not_so_random_number)
-    subject.class.random.should subject.class.file[not_so_random_number]
-  end
-
-  it "should retrieve a phrase from a given user" do
-    phrases = {"quotes" => ["<PotHix> is here", "<agaelebe> I'm a bot", "<qmx> meh."]}
-    YAML.should_receive(:load_file).twice.and_return(phrases)
-    subject.class.random_by_user("PotHix").should eql("<PotHix> is here")
-    subject.class.random_by_user("qmx").should eql("<qmx> meh.")
+    phrases_file["motorcycles"][phrases_file["motorcycles"].size - 1].should == subject.phrase
   end
 end
-
