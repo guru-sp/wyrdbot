82d63a73ffba9877d176fa344003cfdde15bb968
Nando Vieira <fnando.vieira@gmail.com>
2011-08-25 21:03:09 -0300

    Add support for regex search. Fallback to user search.

--- lib/simple_bot/quote.rb
+++ lib/simple_bot/quote.rb
@@ -2,11 +2,22 @@
 module SimpleIrcBot
   class Quote < Phrase
     def self.random_by_user(query)
+      regex = Regexp.new(query) rescue nil
+      (regex ? find_by_regex(regex) : find_by_user(query)).sample
+    end
+
+    private
+    def self.find_by_regex(regex)
+      filter_phrases {|phrase| phrase =~ regex}
+    end
+
+    def self.find_by_user(query)
       user = query.split(" ").first
-      user_phrases = self.file["#{self.class_name}s"].select do |phrase|
-        phrase.match(/<#{user}>/)
-      end
-      user_phrases[rand(user_phrases.size)]
+      filter_phrases {|phrase| phrase.match(/<#{user}>/)}
+    end
+
+    def self.filter_phrases(&block)
+      self.file["#{self.class_name}s"].select(&block)
     end
   end
 end
