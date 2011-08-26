927a158b39f896ab2c0d97d3493d03e48c98ee2f
PotHix <pothix@pothix.com>
2011-08-26 14:18:40 -0300

    Removing porn feature and adding a joke for all the fappers

--- lib/simple_bot/pr0n.rb
+++ lib/simple_bot/pr0n.rb
@@ -1,8 +1,10 @@
+# encoding: utf-8
 module SimpleBot
   class Pr0n
     def self.search(query)
-      video = Redtube::Video.search(query.to_s).sample
-      "#{video.title} - #{video.url} *fap* *fap* *fap*"
+      # video = Redtube::Video.search(query.to_s).sample
+      # "#{video.title} - #{video.url} *fap* *fap* *fap*"
+      "Ahh safadinho! Ta querendo pornografia né?"
     rescue Exception => error
     end
   end
