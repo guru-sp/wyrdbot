e84f4867edee88f90961b1004838ac8839c8ac56
PotHix <pothix@pothix.com>
2011-08-26 22:42:36 -0300

    Changing requires and renaming bot file

--- lib/simple_bot.rb
+++ lib/simple_bot.rb
@@ -1,26 +1,16 @@
 # encoding: utf-8
-require "yaml"
-require "socket"
-require "open-uri"
-require "json"
-require "logger"
-require "daemons"
-require "redtube"
-require "pathname"
+libs = ["yaml", "socket", "open-uri", "json", "logger", "daemons", "redtube", "pathname"]
+libs.each {|file| require file}
 
-require "simple_bot/logger"
-require "simple_bot/ed_robot"
-require "simple_bot/google"
-require "simple_bot/greetings"
-require "simple_bot/utils"
 require "simple_bot/flame_war"
-require "simple_bot/sentence"
-require "simple_bot/quote"
-require "simple_bot/motorcycle"
-require "simple_bot/simple_bot"
-require "simple_bot/redhead"
-require "simple_bot/pr0n"
-require "simple_bot/asian"
+require "simple_bot/greetings"
+require "simple_bot/logger"
+
+require "simple_bot/sentences/sentence"
+Dir[File.dirname(__FILE__) + "/simple_bot/sentences/**/*.rb"].each {|file| require file}
+Dir[File.dirname(__FILE__) + "/simple_bot/services/**/*.rb"].each {|file| require file}
+
+require "simple_bot/bot"
 
 module SimpleBot
   def self.root
