27dd13691bb8b85c548db99d61fbdac56203d83f
PotHix <pothix@pothix.com>
2011-08-30 19:33:16 -0300

    Fixing the spec for agendatech event

--- spec/simple_bot/services/utils_spec.rb
+++ spec/simple_bot/services/utils_spec.rb
@@ -15,11 +15,16 @@ describe "SimpleBot::Utils" do
         "descricao"=>"horário: 10:00"
       }}]
       subject.stub_chain(:open, :read => event_info.to_json)
-      subject.agendatech.should include("17/11/2011 às 00:00")
+      subject.agendatech.should include("17/11/2011 às 10:00")
     end
 
     it "should get the message for 'no events'" do
-      event_info = [{"evento"=>{"estado"=>"RS","nome"=>"RS on Rails","data"=>"2011-11-17T00:00:00-02:00"}}]
+      event_info = [{"evento"=>{
+        "estado"=>"RS",
+        "nome"=>"RS on Rails",
+        "data"=>"2011-11-17T00:00:00-02:00",
+        "descricao"=>""
+      }}]
       subject.stub_chain(:open, :read => event_info.to_json)
       subject.agendatech.should match("Nenhum evento do Guru-SP cadastrado no Agendatech")
     end
