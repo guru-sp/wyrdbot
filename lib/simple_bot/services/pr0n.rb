80bf1e2b25ab943329ca89d087737135c60b8917
Vinicius Baggio <vinibaggio@gmail.com>
2011-10-14 17:47:39 -0300

    Consertando rescue e removendo comentários

--- lib/simple_bot/services/pr0n.rb
+++ lib/simple_bot/services/pr0n.rb
@@ -2,10 +2,8 @@
 module SimpleBot
   class Pr0n
     def self.search(query)
-      # video = Redtube::Video.search(query.to_s).sample
-      # "#{video.title} - #{video.url} *fap* *fap* *fap*"
       "Ahh safadinho! Ta querendo pornografia né?"
-    rescue Exception => error
+    rescue StandardError => error
     end
   end
 end
